96f34bc6a8e18734df8f476c760e417428bf8e28
Milind Jindal <milindjindal@gmail.com>
2015-11-27 14:18:16 -0700

    Added changes to run everything from the WORK_DIRECTORY which contains the CPP and the JAVA files

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 clear
-
+##########################################################################
+# VARIABLES TO BE SET BY THE USER
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/:$JAVA_HOME/jre/lib/amd64/server
-LIBPATH=$JAVA_HOME/jre/lib/amd64/server/
 export WEKA_HOME=/home/milind/Desktop/JNI_Test/weka-3-6-13
-export CLASSPATH=$WEKA_HOME/weka.jar:/home/milind/workspace_robotics/gitRepo/Robotics/
+export WORK_DIRECTORY=/home/milind/workspace_robotics/gitRepo/Robotics/
+##########################################################################
+
+# EXTENDED VARIABLES
+export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/:$JAVA_HOME/jre/lib/amd64/server
+export LIBPATH=$JAVA_HOME/jre/lib/amd64/server/
+export CLASSPATH=$WEKA_HOME/weka.jar:$WORK_DIRECTORY
 export PATH=$PATH:$WEKA_HOME/weka.jar
 
 echo '---------------------------'
-echo 'PATH=' $PATH
-echo 'CLASSPATH='$CLASSPATH
+echo 'JAVA_HOME='       $JAVA_HOME
+echo 'WEKA_HOME='       $WEKA_HOME
+echo 'WORK_DIRECTORY='  $WORK_DIRECTORY
 echo ''
 echo 'Compilation starts Here:'
 echo '---------------------------'
-
+cd $WORK_DIRECTORY
 if [ -a GenericClassifierWeka.class ]
 	then
 		echo 'Compiling Java'
@@ -28,7 +34,7 @@ if [ -a executable ]
 		rm executable
 		
 fi
-g++ -Wno-write-strings -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux/ GenericClassifierWeka.cpp -o executable -L${LIBPATH} -ljvm 
+g++ -Wno-write-strings -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux/ $WORK_DIRECTORY/GenericClassifierWeka.cpp -o $WORK_DIRECTORY/executable -L${LIBPATH} -ljvm 
 
 echo 'Execution starts here:'
 echo '---------------------------'
