cc71555b92289c8727c6994d921882508c483e0e
Milind Jindal <milindjindal@gmail.com>
2015-11-27 20:43:44 -0700

    check if the variables are set or not and exit appropriately

--- run.sh
+++ run.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 clear
+
 ##########################################################################
 # VARIABLES TO BE SET BY THE USER
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export WEKA_HOME=/home/milind/Desktop/JNI_Test/weka-3-6-13
 export WORK_DIRECTORY=/home/milind/workspace_robotics/gitRepo/Robotics/
+export WEKA_HOME=/home/milind/Desktop/JNI_Test/weka-3-6-13
 ##########################################################################
 
 # EXTENDED VARIABLES
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/:$JAVA_HOME/jre/lib/amd64/server
 export LIBPATH=$JAVA_HOME/jre/lib/amd64/server/
-export CLASSPATH=$WEKA_HOME/weka.jar:$WORK_DIRECTORY
+export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/:$JAVA_HOME/jre/lib/amd64/server
+export CLASSPATH=$CLASSPATH:$WORK_DIRECTORY:$WEKA_HOME/weka.jar
 export PATH=$PATH:$WEKA_HOME/weka.jar
 
-echo '---------------------------'
-echo 'JAVA_HOME='       $JAVA_HOME
-echo 'WEKA_HOME='       $WEKA_HOME
-echo 'WORK_DIRECTORY='  $WORK_DIRECTORY
-echo ''
-echo 'Compilation starts Here:'
-echo '---------------------------'
-cd $WORK_DIRECTORY
+if [ -z ${WEKA_HOME} ]; 
+	then 
+		echo "ERROR: WEKA_HOME is not set"; 
+		exit 1;
+	else 
+		echo "INFO: WEKA_HOME:" $CLASSPATH; 	
+fi
+
+if [ -z ${JAVA_HOME} ]; 
+	then 
+		echo "ERROR: JAVA_HOME is not set"; 
+		exit 1;
+	else 
+		echo "INFO: JAVA_HOME:" $JAVA_HOME; 	
+fi
+
+if [ -z ${WORK_DIRECTORY} ]; 
+	then 
+		echo "ERROR: WORK_DIRECTORY is not set"; 
+		exit 1;
+	else 
+		echo "INFO: WORK_DIRECTORY:" $WORK_DIRECTORY; 	
+fi
+
 if [ -a GenericClassifierWeka.class ]
 	then
-		echo 'Compiling Java'
-		rm GenericClassifierWeka.class
-		
+		rm GenericClassifierWeka.class		
 fi
 javac GenericClassifierWeka.java
+
 if [ -a executable ]
 	then
-		echo 'Compiling C++'
-		rm executable
-		
+		rm executable	
 fi
-g++ -Wno-write-strings -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux/ $WORK_DIRECTORY/GenericClassifierWeka.cpp -o $WORK_DIRECTORY/executable -L${LIBPATH} -ljvm 
+g++ -Wno-write-strings -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux/ GenericClassifierWeka.cpp -o executable -L${LIBPATH} -ljvm 
 
-echo 'Execution starts here:'
-echo '---------------------------'
-./executable Arun.model irisData.csv 5
+
+./executable /home/milind/workspace_robotics/CPP/Arun.model /home/milind/workspace_robotics/CPP/irisData.csv 5
